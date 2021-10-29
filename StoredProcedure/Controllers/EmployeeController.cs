d333119178b2b76ede374b66021374567380df8d
Opilane TTHK <opilane@tthk.ee>
2021-10-29 14:07:42 +0300

    C Terav

---
+++ StoredProcedure/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProcedure.Data;
+using StoredProcedure.Models;
+
+namespace StoredProcedure.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public StoredProcDbContext _context;
+
+        public EmployeeController(StoredProcDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employee
+                .FromSqlRaw<Employee>("spSerachEmployees")
+                .ToList();
+
+            return result;
+        }
+    }
+}
---
+++ StoredProcedure/Data/StoredProcDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoredProcedure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoredProcedure.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options) { }
+
+        public DbSet <Employee> Employee {get; set;}
+    }
+}
